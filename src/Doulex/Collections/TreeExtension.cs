a54d8b4d0cdb308a50faa6a9c13de2e8a8b29d4b
nepton <nepton@qq.com>
2022-12-21 17:29:36 +0800

    add ToTree methods publish

---
+++ src/Doulex/Collections/TreeExtension.cs
@@ -0,0 +1,113 @@
+﻿namespace Doulex.Collections
+{
+    /// <summary>
+    /// Enumerable extensions
+    /// </summary>
+    public static class TreeExtension
+    {
+        /// <summary>
+        /// Traverses an object hierarchy and return a flattened list of elements
+        /// based on a predicate.
+        /// </summary>
+        /// <param name="source">The collection of your topmost TSource objects</param>
+        /// <param name="childrenSelector">A function that fetches the child collection from an object.</param>
+        /// <typeparam name="TSource">The type of object in your collection</typeparam>
+        /// <returns>A flattened list of objects which meet the criteria in selectorFunction.</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static IEnumerable<TSource> Flatten<TSource>(
+            this TSource                        source,
+            Func<TSource, IEnumerable<TSource>> childrenSelector)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            if (childrenSelector == null)
+                throw new ArgumentNullException(nameof(childrenSelector));
+
+            return Flatten(new[] {source}, childrenSelector);
+        }
+
+        /// <summary>
+        /// Traverses an object hierarchy and return a flattened list of elements
+        /// based on a predicate.
+        /// </summary>
+        /// <param name="source">The collection of your topmost TSource objects</param>
+        /// <param name="childrenSelector">A function that fetches the child collection from an object.</param>
+        /// <typeparam name="TSource">The type of object in your collection</typeparam>
+        /// <returns>A flattened list of objects which meet the criteria in selectorFunction.</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static IEnumerable<TSource> Flatten<TSource>(
+            this IEnumerable<TSource>           source,
+            Func<TSource, IEnumerable<TSource>> childrenSelector)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            if (childrenSelector == null)
+                throw new ArgumentNullException(nameof(childrenSelector));
+
+            // Add what we have to the stack
+            var sources = source as TSource[] ?? source.ToArray();
+            var result  = sources.AsEnumerable();
+
+            // Go through the input enumerable looking for children,
+            // and add those if we have them
+            foreach (var element in sources)
+            {
+                var children = childrenSelector(element).Flatten(childrenSelector);
+                result = result.Concat(children);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Convert a flatten list to a tree
+        /// </summary>
+        /// <param name="source">The source</param>
+        /// <param name="rootSelector">The root selector</param>
+        /// <param name="idSelector">The id selector</param>
+        /// <param name="parentIdSelector">The parent id selector</param>
+        /// <param name="childrenAssign">The children assign</param>
+        /// <typeparam name="T">The type of the source</typeparam>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static T? ToTree<T>(
+            this IEnumerable<T>       source,
+            Func<IEnumerable<T>, T?>  rootSelector,
+            Func<T, object>           idSelector,
+            Func<T, object>           parentIdSelector,
+            Action<T, IEnumerable<T>> childrenAssign)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (rootSelector == null) throw new ArgumentNullException(nameof(rootSelector));
+            if (idSelector == null) throw new ArgumentNullException(nameof(idSelector));
+            if (parentIdSelector == null) throw new ArgumentNullException(nameof(parentIdSelector));
+            if (childrenAssign == null) throw new ArgumentNullException(nameof(childrenAssign));
+
+            var array  = source as T[] ?? source.ToArray();
+            var lookup = array.ToLookup(parentIdSelector);
+            var root   = rootSelector(array);
+
+            if (root == null)
+            {
+                return default;
+            }
+
+            var stack = new Stack<T>();
+            stack.Push(root);
+            while (stack.Count > 0)
+            {
+                var current  = stack.Pop();
+                var children = lookup[idSelector(current)].ToArray();
+                foreach (var child in children)
+                {
+                    stack.Push(child);
+                }
+
+                childrenAssign(current, children);
+            }
+
+            return root;
+        }
+    }
+}
---
+++ tests/UnitTest.Doulex/Collections/TreeExtensionsTests.cs
@@ -0,0 +1,67 @@
+using System.Security.AccessControl;
+using Doulex.Collections;
+using FluentAssertions;
+using Microsoft.VisualBasic;
+
+namespace UnitTest.Doulex.Collections;
+
+public class TreeExtensionsTests
+{
+    // 测试 TreeExtensions.ToTree 方法
+    [Fact]
+    public void ToTree_WhenCalled_ReturnsTree()
+    {
+        // Arrange
+        var root    = new Node {Id = Guid.NewGuid(), ParentId = default};
+        var child1  = new Node {Id = Guid.NewGuid(), ParentId = root.Id};
+        var child2  = new Node {Id = Guid.NewGuid(), ParentId = root.Id};
+        var child3  = new Node {Id = Guid.NewGuid(), ParentId = child1.Id};
+        var child4  = new Node {Id = Guid.NewGuid(), ParentId = child1.Id};
+        var child5  = new Node {Id = Guid.NewGuid(), ParentId = child2.Id};
+        var child6  = new Node {Id = Guid.NewGuid(), ParentId = child2.Id};
+        var child7  = new Node {Id = Guid.NewGuid(), ParentId = child3.Id};
+        var child8  = new Node {Id = Guid.NewGuid(), ParentId = child3.Id};
+        var child9  = new Node {Id = Guid.NewGuid(), ParentId = child4.Id};
+        var child10 = new Node {Id = Guid.NewGuid(), ParentId = child4.Id};
+
+        var nodes = new List<Node>
+        {
+            root,
+            child1,
+            child2,
+            child3,
+            child4,
+            child5,
+            child6,
+            child7,
+            child8,
+            child9,
+            child10
+        };
+
+        // Act
+        var actual = nodes.ToTree(
+            array => array.FirstOrDefault(x => x.ParentId == default),
+            n => n.Id,
+            n => n.ParentId,
+            (n, c) => n.Children = c.ToArray());
+
+        // Assert
+        Assert.NotNull(actual);
+        Assert.Equal(2, root.Children.Length);
+        Assert.Equal(2, child1.Children.Length);
+        Assert.Equal(2, child2.Children.Length);
+        Assert.Equal(2, child3.Children.Length);
+        Assert.Equal(2, child4.Children.Length);
+
+        var actualList = actual.Flatten(x => x.Children);
+        actualList.Should().BeEquivalentTo(nodes);
+    }
+}
+
+public class Node
+{
+    public Guid   Id       { get; set; }
+    public Guid   ParentId { get; set; }
+    public Node[] Children { get; set; }
+}
