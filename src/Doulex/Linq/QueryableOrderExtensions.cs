58fc01a422a3a3df06835c4a864359fc534fa4ec
nepton <nepton@qq.com>
2022-12-23 10:16:40 +0800

    add order extension publish v1.2.0

---
+++ src/Doulex/Linq/QueryableOrderExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq.Expressions;
+
+namespace Doulex.Linq
+{
+    /// <summary>
+    /// Queryable extension
+    /// </summary>
+    public static class QueryableOrderExtensions
+    {
+        /// <summary>
+        /// Order the query
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="keySelector"></param>
+        /// <param name="isAscending"></param>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TKey"></typeparam>
+        /// <returns></returns>
+        public static IOrderedQueryable<TSource> OrderBy<TSource, TKey>(
+            this IQueryable<TSource>        source,
+            Expression<Func<TSource, TKey>> keySelector,
+            bool                            isAscending)
+        {
+            if (isAscending)
+                return source.OrderBy(keySelector);
+
+            return source.OrderByDescending(keySelector);
+        }
+    }
+}
