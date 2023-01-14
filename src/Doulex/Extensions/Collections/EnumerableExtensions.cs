380622f305bcf6f2d9d0eb2135b912fbadb83a6b
nepton <nepton@qq.com>
2023-01-14 14:12:19 +0800

    publish

--- src/Doulex/Extensions/Collections/EnumerableExtensions.cs
+++ src/Doulex/Extensions/Collections/EnumerableExtensions.cs
@@ -11,5 +11,9 @@ public static class EnumerableExtensions
     /// <typeparam name="T">The type of the collection.</typeparam>
     /// <param name="enumerable">The collection to check.</param>
     /// <returns>True if collection is null or empty, false otherwise.</returns>
+#if NETSTANDARD2_1_OR_GREATER
+    public static bool IsNullOrEmpty<T>([NotNullWhen(false)] this IEnumerable<T>? enumerable) => enumerable == null || !enumerable.Any();
+#else
     public static bool IsNullOrEmpty<T>(this IEnumerable<T>? enumerable) => enumerable == null || !enumerable.Any();
+#endif
 }
