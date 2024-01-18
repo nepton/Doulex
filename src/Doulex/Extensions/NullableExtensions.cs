472a3edd1b3a4c208593824a3b7435d69fc78695
nepton <nepton@qq.com>
2024-01-18 09:18:27 +0800

    publish 5.9

--- src/Doulex/Extensions/NullableExtensions.cs
+++ src/Doulex/Extensions/NullableExtensions.cs
@@ -15,4 +15,19 @@ public static class NullableExtensions
     {
         return value.HasValue ? func(value.Value) : value;
     }
+
+    /// <summary>
+    /// Gets the value or throws an exception if the value is null.
+    /// </summary>
+    /// <param name="nullable">The nullable.</param>
+    /// <typeparam name="T">Type of the nullable.</typeparam>
+    /// <returns>The value of the nullable.</returns>
+    /// <exception cref="ArgumentNullException">If the nullable is null.</exception>
+    public static T GetValueOrThrow<T>(this T? nullable) where T : struct
+    {
+        if (nullable.HasValue)
+            return nullable.Value;
+
+        throw new ArgumentNullException(nameof(nullable));
+    }
 }
